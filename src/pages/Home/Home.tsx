2b4a257e7323898d09814dbd1c7b73ba2005f82c
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-07-31 00:38:28 +0800

    add loading animation when calling apis

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -39,6 +39,8 @@ function Home() {
     const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
     const [bucketNameDeleted, setbucketNameDeleted] = useState('');
     const [warningEmpty, setWarningEmpty] = useState(false);
+    const [loadingAdd, setLoadingAdd] = useState(false);
+    const [loadingDelete, setLoadingDelete] = useState(false);
 
     
 
@@ -61,6 +63,7 @@ function Home() {
     };
 
     const handleAddBucket = async () => {
+        setLoadingAdd(true);
         if (bucketName == "" || bucketName == ''){
             setWarningEmpty(true)
         }
@@ -95,10 +98,12 @@ function Home() {
                 setBucketList(tempArray)
             }
             setIsModalOpen(false)
+            setLoadingAdd(false);
         }
     }
 
     const handleDeleteBucket = async () => {
+        setLoadingDelete(true);
         const token = localStorage.getItem('authToken');
         const response = await fetch('/.netlify/functions/delete_bucket', {
             method: 'POST',
@@ -130,6 +135,7 @@ function Home() {
         }
         setbucketNameDeleted('')
         setIsModalDeleteOpen(false)
+        setLoadingDelete(false);
     }
 
     const handleBucketClick = (name: string) => {
@@ -143,6 +149,7 @@ function Home() {
             }
         })
     }
+    
     
 
     return(
@@ -184,13 +191,24 @@ function Home() {
                                 <p className="text-red-600 text-xs mb-3 font-bold">Bucket name must not be empty!</p>
                             )}
                         
-                            <div className="flex justify-between">
+                            <div className="flex justify-between mt-2">
                                 <button onClick={handleCloseAddNewBucketWindow} className="text-2xs bg-amber-800 text-white px-4 py-2 rounded font-bold">
                                     Cancel
                                 </button>
-                                <button onClick={handleAddBucket} className="bg-amber-800 text-white text-2xs px-4 py-2 rounded font-bold">
-                                    Add
-                                </button>
+                                {loadingAdd ?
+                                    (
+                                        <div role="status">
+                                        <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-amber-950" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                        </svg>
+                                        <span className="sr-only">Loading...</span>
+                                    </div>
+                                    )
+                                    : ( <button onClick={handleAddBucket} className="bg-amber-800 text-white text-2xs px-4 py-2 rounded font-bold">
+                                            Add
+                                        </button>
+                                )}
                             </div>
                         </div>
                     </div>
@@ -205,9 +223,20 @@ function Home() {
                                 <button onClick={handleCloseDeleteBucketWindow} className="text-2xs bg-amber-800 text-white px-4 py-2 rounded font-bold">
                                     Cancel
                                 </button>
-                                <button onClick={handleDeleteBucket} className="bg-amber-800 text-white text-2xs px-4 py-2 rounded font-bold">
-                                    Delete
-                                </button>
+                                {loadingDelete ?
+                                    (
+                                        <div role="status">
+                                        <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-amber-950" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                        </svg>
+                                        <span className="sr-only">Loading...</span>
+                                    </div>
+                                    )
+                                    : ( <button onClick={handleDeleteBucket} className="bg-amber-800 text-white text-2xs px-4 py-2 rounded font-bold">
+                                            Delete
+                                        </button>
+                                )}
                             </div>
                         </div>
                     </div>
